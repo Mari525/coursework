e11a64b04cb5af384f7791e23fbf5edeca332aa3
Mari <sherst.neva.maria@ya.ru>
2021-08-02 13:33:39 +0300

    доделаны страницы входа и регистрации

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-// $(document).ready(function() {
-//         $('.categories__button').focus(function () {
-//                 $('.categories__list').toggleClass('categories__list--opened');
-//         })
+$(document).ready(function() {
+        $('.categories__button').focus(function () {
+                $('.categories__list').toggleClass('categories__list--opened');
+        })
 
-//         $('.profile').click(function () {
-//                 $('.profile__list').toggleClass('profile__list--closed');
-//         })
+        $('.profile').click(function () {
+                $('.profile__list').toggleClass('profile__list--closed');
+        })
 
-//         $('.profile__button').focus(function () {
-//                 $('.profile__list').toggleClass('profile__list--closed');
-//         })
-// });
+        $('.profile__button').focus(function () {
+                $('.profile__list').toggleClass('profile__list--closed');
+        })
+});
 
 // let categoriesButton = document.getElementById("categories-button");
 // let categoriesList = document.getElementById("categories-list");
