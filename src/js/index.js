9df166fc8275e96884b73a49d0fad0e2520b3613
Mari <sherst.neva.maria@ya.ru>
2021-10-04 00:01:48 +0300

    fin

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,61 @@
 $(document).ready(function() {
-        $('.categories__button').focus(function () {
-                $('.categories__list').toggleClass('categories__list--opened');
-        })
+	$('.categories__button').focus(function () {
+		$('.categories__list').toggleClass('categories__list--opened');
+	})
 
-        $('.profile').click(function () {
-                $('.profile__list').toggleClass('profile__list--closed');
-        })
+	$('.profile').click(function () {
+		$('.profile__list').toggleClass('profile__list--closed');
+	})
 
-        $('.profile__button').focus(function () {
-                $('.profile__list').toggleClass('profile__list--closed');
-        })
+	$('.profile__button').focus(function () {
+		$('.profile__list').toggleClass('profile__list--closed');
+	})
 });
 
-// let categoriesButton = document.getElementById("categories-button");
-// let categoriesList = document.getElementById("categories-list");
 
-// categoriesButton.addEventListener("mouseenter", function(){
+import axios from "axios";
+
+const formLogin = document.querySelector(".signin__form");
+const inputLogin = formLogin.querySelector(".signin__input--login");
+const inputPassword = formLogin.querySelector(".signin__input--password");
+
+const ApiInformation = {
+        BASE_URL: "http://delivery.std-1372.ist.mospolytech.ru",
+        TIMEOUT: 5000
+};
+
+const createAPI = () => {
+        const api = axios.create({
+                baseURL: ApiInformation.BASE_URL,
+                timeout: ApiInformation.TIMEOUT
+        });
         
-// })
+        const onSuccess = (response) => response;
+        
+        const onFail = (err) => {
+                const {response} = err;
+                throw err;
+        };
+        
+        api.interceptors.response.use(onSuccess, onFail);
+        
+        return api;
+        };
+
+const api = createAPI();
+
+const login = (email, password) => {
+        console.log(email, password)
+        api.post('/user/login/', {email, password})
+        .then((response) => console.log(response))
+        .catch((err) => console.log(err));
+};
+
+const onFormSubmit = (evt) => {
+        evt.preventDefault();
+        const loginValue = inputLogin.value;
+        const passwordValue = inputPassword.value;
+        login(loginValue, passwordValue);
+}
+
+formLogin.addEventListener('submit', onFormSubmit);
